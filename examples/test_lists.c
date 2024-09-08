a041d48b63937e1fde02edfaa6b2650e81b01346
Navid Dezashibi <dezashibi@gmail.com>
2024-09-08 13:48:50 +0400

    proper comments

--- examples/test_lists.c
+++ examples/test_lists.c
@@ -1,10 +1,10 @@
 // ***************************************************************************************
-//    Project:
-//    File: test_lists.c
+//    Project: dcommon -> https://github.com/dezashibi-c/dcommon
+//    File: test_lists.h
 //    Date: 2024-09-08
 //    Author: Navid Dezashibi
 //    Contact: navid@dezashibi.com
-//    Website:
+//    Website: https://dezashibi.com | https://github.com/dezashibi
 //    License:
 //     Please refer to the LICENSE file, repository or website for more
 //     information about the licensing of this work. If you have any questions
