031edbc4056ab35712669b61423d6e7c73a6c0a2
Navid Dezashibi <dezashibi@gmail.com>
2024-09-12 19:20:29 +0400

    more tests

--- examples/test_string_view.c
+++ examples/test_string_view.c
@@ -17,24 +17,39 @@
 #define DCOMMON_IMPL
 #include "../src/dcommon/dcommon.h"
 
+void print_sv(DCStringView* sv)
+{
+    printf("I can see '" DC_SV_FMT "' in the text!\n", dc_sv_fmt_val(*sv));
+}
+
 int main()
 {
     string my_string = "Hello, World!";
 
     DCStringView view = dc_sv_create(my_string, 7, 5);
+    DCStringView view2 = dc_sv_create(my_string, 0, 5);
 
-    printf("I can see '" DC_SV_FMT "' in the text!\n", dc_sv_fmt_val(view));
+    print_sv(&view);
+    print_sv(&view2);
 
     string world = dc_sv_as_cstr(&view);
+    string hello = dc_sv_as_cstr(&view2);
 
     // do whatever I like with this, because it's now extracted and saved in
     // view.cstr
     printf("yes '%s' one more time!\n", world);
+    printf("yes '%s' one more time!\n", hello);
 
     if (dc_sv_cmp(view, "World") == 0)
+        printf("Yes for heaven sake, they are the same!\n");
+
+    if (dc_sv_cmp(view2, "Hello") == 0)
         printf("Yes for heaven sake, they are the same!\n");
 
     // don't forget to free it!
     dc_sv_free(&view);
     // free(world); // or this (just one of them of course)
+
+    dc_sv_free(&view2);
+    // free(hello); // or this (just one of them of course)
 }
