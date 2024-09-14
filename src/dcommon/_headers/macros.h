0219f4e6b14e4c193cca675240635defbd827f8a
Navid Dezashibi <dezashibi@gmail.com>
2024-09-14 12:26:51 +0400

    rename

--- src/dcommon/_headers/macros.h
+++ src/dcommon/_headers/macros.h
@@ -47,7 +47,7 @@
         }                                                                      \
     } while (0)
 
-#define tostr_enum_scase(ITEM)                                                 \
+#define dc_make_tostr_enum_case(ITEM)                                          \
     case ITEM:                                                                 \
         return #ITEM
 
